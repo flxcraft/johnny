c49b57a7592b205e38e9b18999fd98a41f351f6c
flxcraft <flxcraft@heidischmal4.de>
2026-04-21 16:49:59 +0000

    Deploying to gh-pages from @ flxcraft/johnny@2bddba22c1600cb5467054506d27c997ca602c4c 🚀

--- preview/version.js
+++ preview/version.js
@@ -1,5 +1,5 @@
 window.BUILD_INFO = {
-  "version": "e0783b5682bd966c7235fd92bbb93d44661f425a",
+  "version": "2bddba22c1600cb5467054506d27c997ca602c4c",
   "channel": "preview",
-  "buildTime": "2026-04-13T14:04:39Z"
+  "buildTime": "2026-04-21T16:49:48Z"
 };
